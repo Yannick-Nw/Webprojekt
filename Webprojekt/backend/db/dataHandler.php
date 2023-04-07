a6ea8e5dd5077774efc2bf27555c156a3bf152a2
TheRoninski <121442902+TheRoninski@users.noreply.github.com>
2023-04-07 17:04:54 +0200

    edit

--- Webprojekt/backend/db/dataHandler.php
+++ Webprojekt/backend/db/dataHandler.php
@@ -1,73 +1,107 @@
 <?php
-include("./models/appointment.php");
 
-class DataHandler
-{
-    private $appointments;
+require_once ("models/appointment.php");
+require_once ("user.php");
+require_once ("config.php");
 
-    public function __construct()
-    {
-        $this->appointments = $this->getDemoData();
+class DataHandler {
+    private $conn;
+
+    public function __construct() {
+        global $config;
+
+        $this->conn = new mysqli($config['host'], $config['username'], $config['password'], $config['dbname']);
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
     }
 
-    public function queryAppointments()
-    {
-        return $this->appointments;
-    }
+    public function queryAppointments() {
+        $appointments = array();
 
-    public function queryAppointmentById($id)
-    {
-        $result = array();
-        foreach ($this->appointments as $val) {
-            if ($val[0]->id == $id) {
-                array_push($result, $val);
+        $query = "SELECT * FROM appointments";
+        $result = mysqli_query($this->conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $appointment = new Appointment(
+                    $row["id"],
+                    $row["title"],
+                    $row["location"],
+                    $row["start_date"],
+                    $row["end_date"],
+                    $row["voting_end_date"]
+                );
+                array_push($appointments, $appointment);
             }
         }
-        return $result;
+
+        return $appointments;
     }
 
-    public function createAppointment($title, $location, $start, $end)
-    {
-        $id = count($this->appointments) + 1;
-        $appointment = new Appointment($id, $title, $location, $start, $end);
-        array_push($this->appointments, array($appointment));
-        return $appointment;
-    }
+    public function queryAppointmentUsers($appointment_id) {
+        $appointment_users = array();
 
-    public function updateAppointment($id, $title, $location, $start, $end)
-    {
-        foreach ($this->appointments as $val) {
-            if ($val[0]->id == $id) {
-                $val[0]->title = $title;
-                $val[0]->location = $location;
-                $val[0]->start_time = $start;
-                $val[0]->end_time = $end;
-                return $val[0];
+        $query = "SELECT * FROM appointments_users WHERE appointment_id = " . $appointment_id;
+        $result = mysqli_query($this->conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $appointment_user = array(
+                    "id" => $row["id"],
+                    "appointment_id" => $row["appointment_id"],
+                    "user_id" => $row["user_id"],
+                    "selected_date" => $row["selected_date"],
+                    "comment" => $row["comment"]
+                );
+                array_push($appointment_users, $appointment_user);
             }
         }
-        return null;
+
+        return $appointment_users;
     }
 
-    public function deleteAppointment($id)
-    {
-        foreach ($this->appointments as $key => $val) {
-            if ($val[0]->id == $id) {
-                unset($this->appointments[$key]);
-                return true;
+    public function queryAvailableDates($appointment_id) {
+        $available_dates = array();
+
+        $query = "SELECT * FROM available_dates WHERE appointment_id = " . $appointment_id;
+        $result = mysqli_query($this->conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $available_date = array(
+                    "id" => $row["id"],
+                    "appointment_id" => $row["appointment_id"],
+                    "date" => $row["date"]
+                );
+                array_push($available_dates, $available_date);
             }
         }
-        return false;
+
+        return $available_dates;
     }
 
-    private function getDemoData()
-    {
-        $demodata = [
-            [new Appointment(1, "Team meeting", "Conference room", "2023-04-12 10:00:00", "2023-04-12 11:00:00")],
-            [new Appointment(2, "Project review", "Online", "2023-04-15 14:00:00", "2023-04-15 16:00:00")],
-            [new Appointment(3, "Client presentation", "Client office", "2023-04-20 09:00:00", "2023-04-20 11:00:00")],
-            [new Appointment(4, "Training session", "Training room", "2023-04-25 13:00:00", "2023-04-25 15:00:00")],
-        ];
-        return $demodata;
-    }
+    /*public function queryUsers() {
+        $users = array();
+
+        $query = "SELECT * FROM users";
+        $result = mysqli_query($this->conn, $query);
+
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $user = new user(
+                    $row["id"],
+                    $row["username"]
+                );
+                array_push($users, $user);
+            }
+        }
+
+        return $users;
+    }*/
+
+    // Restliche Methoden ...
 }
+
 ?>
