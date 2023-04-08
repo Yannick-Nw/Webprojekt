e91184032db0830e14537106108b0894c74c2b73
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-04-08 16:46:00 +0200

    servicehandler

--- Webprojekt/backend/serviceHandler.php
+++ Webprojekt/backend/serviceHandler.php
@@ -1,5 +1,15 @@
 <?php 
 require_once ("db/dataHandler.php");
 $result = new DataHandler();
-$appointments = $result->queryAppointments();
-echo json_encode($appointments);
+if (!empty($_GET['param'])) {
+    $appointmentDetails = $_GET['param'];
+    if ($appointmentDetails == 'detail') {
+        $users = $result->queryAppointmentUsers($appointmentDetails);
+        echo json_encode($users);
+        $details = $result->queryAvailableDates($appointmentDetails);
+        echo json_encode($details);
+    } elseif ($appointmentDetails == 'main') {
+        $appointments = $result->queryAppointments();
+        echo json_encode($appointments);
+    }
+}
--- Webprojekt/frontend/js/script.js
+++ Webprojekt/frontend/js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    $("#tableRows").children().click(function () {
-        $("#allAppointments").hide();
-        $("#appointmentDetails").show();
-    });
+    loaddata('main');
+    
     $("#cancelDetails").click(function () {
         $("#appointmentDetails").hide();
         $("#allAppointments").show();
@@ -16,5 +14,107 @@ $(document).ready(function () {
         $("#allAppointments").show();
     });
 });
+//$("p").children().on("click", function()
+//data: {param: }
 
-  //$("p").children().on("click", function()
+function loaddata(requestTyp) {
+    $.ajax({
+        url: "../backend/serviceHandler.php",
+        data: {param: requestTyp},
+        dataType: "json",
+        success: function (response) {
+            for (var i = 0; i < response.length; i++) {
+                var appointmentObjekt = response[i];
+                var id = appointmentObjekt.id;
+                var title = appointmentObjekt.title;
+                var location = appointmentObjekt.location;
+                var start_time = appointmentObjekt.start_time;
+                var end_time = appointmentObjekt.end_time;
+                
+                var tbody = $('#tableRows');    // Zugriff auf das tbody-Element
+                
+                var tr = $('<tr></tr>').attr('id',id).addClass('h-12 hover:bg-amber-950/50');   // Erstellen einer neuen Zeile
+                
+                var td1 = $('<td></td>').text(title); // Erstellen und Hinzufügen von Zellen zur Zeile
+                tr.append(td1);
+                
+                var td2 = $('<td></td>').addClass('max-md:hidden').text('Meine Personen');
+                tr.append(td2);
+                
+                var td3 = $('<td></td>').addClass('max-md:hidden').text(start_time);
+                tr.append(td3);
+                
+                var td4 = $('<td></td>').addClass('max-md:hidden').text(end_time);
+                tr.append(td4);
+                
+                var td5 = $('<td></td>').text('Mein Status');
+                tr.append(td5);
+                
+                tbody.append(tr);   // Hinzufügen der Zeile zur Tabelle
+                
+            }
+            
+            $("#tableRows").children().click(function () {
+                $("#allAppointments").hide();
+                appointmentChoice(this);
+                $("#appointmentDetails").show();
+            });
+            
+        },
+        error: function() {
+            console.log('Ein Fehler ist aufgetreten: ')
+        }
+        
+    });
+}
+
+function appointmentChoice(choice){
+    $.ajax({
+        url: "../backend/serviceHandler.php",
+        data: {param: requestTyp},
+        dataType: "json",
+        success: function (response) {
+            for (var i = 0; i < response.length; i++) {
+                var appointmentObjekt = response[i];
+                var id = appointmentObjekt.id;
+                var appointment_id = appointmentObjekt.appointment_id;
+                var user_id = appointmentObjekt.user_id;
+                var selected_date = appointmentObjekt.selected_date;
+                var comment = appointmentObjekt.comment;
+                
+                var tr = $('#appointmentDates');    // Zugriff auf das tbody-Element
+                
+                var tr = $('<tr></tr>').attr('id',id).addClass('h-12 hover:bg-amber-950/50');   // Erstellen einer neuen Zeile
+                
+                var td1 = $('<td></td>').text(title); // Erstellen und Hinzufügen von Zellen zur Zeile
+                tr.append(td1);
+                
+                var td2 = $('<td></td>').text('Meine Personen');
+                tr.append(td2);
+                
+                var td3 = $('<td></td>').text(start_time);
+                tr.append(td3);
+                
+                var td4 = $('<td></td>').text(end_time);
+                tr.append(td4);
+                
+                var td5 = $('<td></td>').text('Mein Status');
+                tr.append(td5);
+                
+                tr.append(tr);   // Hinzufügen der Zeile zur Tabelle
+                
+            }
+            
+            $("#tableRows").children().click(function () {
+                $("#allAppointments").hide();
+                appointmentChoice(this);
+                $("#appointmentDetails").show();
+            });
+            
+        },
+        error: function() {
+            console.log('Ein Fehler ist aufgetreten: ')
+        }
+        
+    });
+}
