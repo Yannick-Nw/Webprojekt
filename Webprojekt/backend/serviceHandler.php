762b60d6b0b72cc5359efd354d605d07e6e1741a
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-04-22 15:40:27 +0200

    servicehandler update

--- Webprojekt/backend/serviceHandler.php
+++ Webprojekt/backend/serviceHandler.php
@@ -4,16 +4,28 @@ $result = new DataHandler();
 if (!empty($_GET['param'])) {
     $requestTyp = $_GET['param'];
     if ($requestTyp == 'main') {
-        $appointments = $result->queryAppointments();
-        //$merged_array = array_merge($appointments, $appointments);
+        $appointments = $result->queryAppointments(["id", "title", "location", "duration", "voting_end_date", "actual_appointment"]);
+        //$participants = $result->queryParticipants();
+        //$merged_array = array_merge($appointments, $participants);
         echo json_encode($appointments);
     } elseif ($requestTyp == 'details') {
         if (!empty($_GET['id'])) {
             $appointmentID = $_GET['id'];
+            $appointments = $result->queryAppointments(["title", "description"], $appointmentID);
             $details = $result->queryDates($appointmentID);
-            $participants = $result->queryAppointmentParticipants($appointmentID);
-            $merged_array = array_merge($details, $participants);
+            $vote = $result->queryAppointmentParticipants($appointmentID);
+            $participants = $result->queryParticipants($appointmentID);
+            $merged_array = array_merge($appointments, $details, $participants, $vote);
+            //print_r($merged_array);
+            header("Content-Type: application/json");
             echo json_encode($merged_array);
         }
     }
+} else {
+    // HTTP-Statuscode auf 400 (Bad Request) setzen
+    http_response_code(400);
+
+    // Fehlermeldung zurückgeben
+    echo "Fehler: Falscher Parameter übergeben";
+    exit;
 }
