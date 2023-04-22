cbe5ccdbab156ddbfe6856931aff6c419af9742e
TheRoninski <121442902+TheRoninski@users.noreply.github.com>
2023-04-22 16:40:43 +0200

    edit

--- Webprojekt/backend/db/dataHandler.php
+++ Webprojekt/backend/db/dataHandler.php
@@ -21,32 +21,6 @@ class DataHandler
         }
     }
 
-    /*  public function queryAppointments() {
-        $appointments = array();
-    
-        $query = "SELECT * FROM appointments";
-    
-        $result = mysqli_query($this->conn, $query);
-    
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $appointment = array(
-                    "id" => $row["id"],
-                    "title" => $row["title"],
-                    "location" => $row["location"],
-                    "description" => $row["description"],
-                    "duration" => $row["duration"],
-                    "voting_end_date" => $row["voting_end_date"],
-                    "actual_appointment" => $row["actual_appointment"]
-                );
-                array_push($appointments, $appointment);
-            }
-        }
-    
-        return $appointments;
-    }
-*/
-
     public function queryAppointments($fields = [], $appointment_id = null)
     {
         $appointments = array();
@@ -73,62 +47,17 @@ class DataHandler
         return $appointments;
     }
 
-
-
-    /*public function updateAppointment($id, $title, $location, $description, $duration, $voting_end_date, $actual_appointment)
-    {
-        $query = "UPDATE appointments SET title='$title', location='$location', description='$description', duration='$duration', voting_end_date='$voting_end_date' actual_appointment='$actual_appointment' WHERE id='$id'";
-        return mysqli_query($this->conn, $query);
-    }
+    
 
     public function deleteAppointment($id)
     {
-        $query = "DELETE FROM appointments WHERE id='$id'";
-        return mysqli_query($this->conn, $query);
-    }*/
-
-    public function updateAppointment($id, $title, $location, $description, $duration, $voting_end_date, $actual_appointment)
-{
-    $stmt = $this->conn->prepare("UPDATE appointments SET title=?, location=?, description=?, duration=?, voting_end_date=?, actual_appointment=? WHERE id=?");
-    $stmt->bind_param("ssssssi", $title, $location, $description, $duration, $voting_end_date, $actual_appointment, $id);
-    return $stmt->execute();
-}
-
-public function deleteAppointment($id)
-{
     $stmt = $this->conn->prepare("DELETE FROM appointments WHERE id=?");
     $stmt->bind_param("i", $id);
     return $stmt->execute();
-}
-
-
-    /*public function queryAppointmentParticipants($appointment_id)
-    {
-        $participants = array();
-
-        $query = "SELECT * FROM appointment_participants WHERE appointment_id=$appointment_id";
-
-        $result = mysqli_query($this->conn, $query);
-
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $participant = array(
-                    "id" => $row["id"],
-                    "appointment_id" => $row["appointment_id"],
-                    "date_id" => $row["date_id"],
-                    "participant_id" => $row["participant_id"],
-                    "vote" => $row["vote"]
-                );
-                array_push($participants, $participant);
-            }
-        }
-
-        return $participants;
-    }*/
-
+    }
 
     public function queryAppointmentParticipants($appointment_id)
-{
+    {
     $participants = array();
 
     $query = "SELECT * FROM appointment_participants WHERE appointment_id = ?";
@@ -151,26 +80,10 @@ public function deleteAppointment($id)
     }
 
     return $participants;
-}
-
-
-    /*public function insertAppointmentParticipant($appointment_id, $participant_id, $vote)
-    {
-        $query = "INSERT INTO appointment_participants (appointment_id, participant_id, vote) VALUES ('" . $appointment_id . "', '" . $participant_id . "', '" . $vote . "')";
-
-        $result = mysqli_query($this->conn, $query);
-
-        if ($result) {
-            return true;
-        } else {
-            return false;
-        }
-    }*/
-
-
+    }
 
     public function insertAppointmentParticipant($appointment_id, $participant_id, $vote)
-{
+    {
     $stmt = $this->conn->prepare("INSERT INTO appointment_participants (appointment_id, participant_id, vote) VALUES (?, ?, ?)");
     $stmt->bind_param("iii", $appointment_id, $participant_id, $vote);
     $result = $stmt->execute();
@@ -181,34 +94,10 @@ public function deleteAppointment($id)
     } else {
         return false;
     }
-}
+    }
 
-
-    /*public function queryDates($appointment_id)
-    {
-        $dates = array();
-
-        $query = "SELECT * FROM dates WHERE appointment_id = " . $appointment_id;
-        $result = mysqli_query($this->conn, $query);
-
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $date = array(
-                    "id" => $row["id"],
-                    "appointment_id" => $row["appointment_id"],
-                    "date" => $row["date"],
-                    "time" => $row["time"]
-                );
-                array_push($dates, $date);
-            }
-        }
-
-        return $dates;
-    }*/
-
-    
     public function queryDates($appointment_id)
-{
+    {
     $dates = array();
 
     $stmt = $this->conn->prepare("SELECT * FROM dates WHERE appointment_id = ?");
@@ -229,87 +118,21 @@ public function deleteAppointment($id)
     }
 
     return $dates;
-}
-
-    
-    
-    /*public function insertDate($appointment_id, $date, $time)
-    {
-        $query = "INSERT INTO dates (appointment_id, date, time) VALUES ($appointment_id, '$date', '$time')";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
     }
-
-    public function updateDate($date_id, $appointment_id, $date, $time)
-    {
-        $query = "UPDATE dates SET appointment_id = $appointment_id, date = '$date', time = '$time' WHERE id = $date_id";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
-    }
-
-    public function deleteDate($date_id)
-    {
-        $query = "DELETE FROM dates WHERE id = $date_id";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
-    }*/
-
-
 
     public function insertDate($appointment_id, $date, $time)
-{
+    {
     $stmt = $this->conn->prepare("INSERT INTO dates (appointment_id, date, time) VALUES (?, ?, ?)");
     $stmt->bind_param("iss", $appointment_id, $date, $time);
     $result = $stmt->execute();
     $stmt->close();
     return $result;
-}
+    }
 
-public function updateDate($date_id, $appointment_id, $date, $time)
-{
-    $stmt = $this->conn->prepare("UPDATE dates SET appointment_id = ?, date = ?, time = ? WHERE id = ?");
-    $stmt->bind_param("isss", $appointment_id, $date, $time, $date_id);
-    $result = $stmt->execute();
-    $stmt->close();
-    return $result;
-}
-
-public function deleteDate($date_id)
-{
-    $stmt = $this->conn->prepare("DELETE FROM dates WHERE id = ?");
-    $stmt->bind_param("i", $date_id);
-    $result = $stmt->execute();
-    $stmt->close();
-    return $result;
-}
-
-
-    /*public function queryParticipants($appointment_id)
-    {
-        $participants = array();
-
-        $query = "SELECT id, appointment_id, username, comment FROM participants WHERE appointment_id = $appointment_id";
-
-        $result = mysqli_query($this->conn, $query);
-
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $participant = array(
-                    "id" => $row["id"],
-                    "appointment_id" => $row["appointment_id"],
-                    "username" => $row["username"],
-                    "comment" => $row["comment"]
-                );
-                array_push($participants, $participant);
-            }
-        }
-
-        return $participants;
-    }*/
-
+    
 
     public function queryParticipants($appointment_id)
-{
+    {                                               
     $participants = array();
     $stmt = $this->conn->prepare("SELECT id, appointment_id, username, comment FROM participants WHERE appointment_id = ?");
     $stmt->bind_param("i", $appointment_id);
@@ -329,34 +152,7 @@ public function deleteDate($date_id)
     }
 
     return $participants;
-}
-
-
-    /*public function insertParticipant($appointment_id, $username)
-    {
-        $query = "INSERT INTO participants (appointment_id, username, vote_response, comment) VALUES ($appointment_id, '$username', NULL, NULL)";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
     }
-
-    public function updateParticipant($participant_id, $selected_date, $comment, $vote_response)
-    {
-        $query = "UPDATE participants SET selected_date='$selected_date', comment='$comment', vote_response='$vote_response' WHERE id = $participant_id";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
-    }
-
-    public function deleteParticipant($participant_id)
-    {
-        $query = "DELETE FROM participants WHERE id = $participant_id";
-        $result = mysqli_query($this->conn, $query);
-        return $result;
-    }
-
-    function __destruct()
-    {
-        mysqli_close($this->conn);
-    }*/
 
 
 
@@ -365,24 +161,6 @@ public function deleteDate($date_id)
     $query = "INSERT INTO participants (appointment_id, username, comment) VALUES (?, ?, NULL)";
     $stmt = mysqli_prepare($this->conn, $query);
     mysqli_stmt_bind_param($stmt, 'is', $appointment_id, $username);
-    $result = mysqli_stmt_execute($stmt);
-    return $result;
-}
-
-public function updateParticipant($participant_id, $selected_date, $comment)
-{
-    $query = "UPDATE participants SET selected_date=?, comment=? WHERE id = ?";
-    $stmt = mysqli_prepare($this->conn, $query);
-    mysqli_stmt_bind_param($stmt, 'ssi', $selected_date, $comment, $participant_id);
-    $result = mysqli_stmt_execute($stmt);
-    return $result;
-}
-
-public function deleteParticipant($participant_id)
-{
-    $query = "DELETE FROM participants WHERE id = ?";
-    $stmt = mysqli_prepare($this->conn, $query);
-    mysqli_stmt_bind_param($stmt, 'i', $participant_id);
     $result = mysqli_stmt_execute($stmt);
     return $result;
 }
